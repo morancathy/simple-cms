9dae0830fac29e465d6e45deb64ce8a750b12d25
Cat <cathy.moran@teladochealth.com>
2022-03-16 18:22:44 -0500

    create new view/route/controller

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,8 +2,17 @@ class DemoController < ApplicationController #DemoController inherits from Appli
   
   layout false  #turns layout off
   def index  #method aka action, renders a view and default view layout
-
+            #default tells it to go to index
+    render('index')  #tyhis is default action. This sets the template. Don't need to do this
+                      #remember, it is not calling this actiion, just setting template.
+                      # render usually used with if/else
   end
+
+  def hello
+    render('hello')  #this is default action. This sets the tempate.
+  end 
 end
 
 # route will be demo/index. Can see this in config/routes
+# Dont need to, but good practice to have action defined for each tempalte. 
+#reminds us its a possible choice server can make w/out serving an error
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  
+  #ROOT route   The # isnt telling it to match it like in simple. 
+                  #This is telling it which controller and which action
+  root 'demo#index'
+
+  #simple route example
   get 'demo/index'
+  get 'demo/hello'
+
+  
+  #default route example (may go away in future versions of rails)
+  # get ':controller(/:action(/:id))'
+
+  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
