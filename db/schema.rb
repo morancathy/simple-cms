ae07e8daa8c65ee676c8a7ecfefcb51c021a1849
Cat <cathy.moran@teladochealth.com>
2022-04-15 23:46:03 -0500

    add schema, migrations and seed (seed did not work)

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
-
+ActiveRecord::Schema.define(version: 2022_04_15_214247) do
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
