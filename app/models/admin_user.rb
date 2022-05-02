f316c6f0f559eb1ce9d0e9fce27b309a7782f228
Cat <cathy.moran@teladochealth.com>
2022-05-02 09:13:15 -0500

    traverse a rich association using has_many :through (though didn't work on console)

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,7 +1,9 @@
 class AdminUser < ApplicationRecord
   # self.table_name = "admin_users"    could have done it this way instead of changing class and file name
   has_and_belongs_to_many :pages
+
   has_many :section_edits
+  has_many :sections, :through => :section_edits
   
   has_secure_password
 end
